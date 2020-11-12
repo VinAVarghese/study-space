0d12b7d25a0bb71802a16b13d595fd0a63d233bb
Vins MacBook <vinavarghese@gmail.com>
2020-11-11 16:23:47 -0800

    added FizzBuzz in java

---
+++ FizzBuzz.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+
+        // Initializing Scanner Object (to read input)
+        Scanner scanner = new Scanner(System.in);
+
+        // Welcome Print out
+        System.out.println("Welcome To Java FizzBuzz");
+
+        // Number Input
+        System.out.print("Enter Number: ");
+        int number = scanner.nextInt();
+
+        // If Statement Determining Output
+        if (number % 5 == 0 && number % 3 == 0){
+            System.out.println("FizzBuzz");
+        } else if (number % 3 == 0) {
+            System.out.println("Buzz");
+        } else if (number % 5 == 0) {
+            System.out.println("Fizz");
+        } else {
+            System.out.println(number);
+        }
+    }
+}
