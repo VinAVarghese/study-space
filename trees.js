4d5d7377c3f3e036d0305fa48c6148fddab4f681
Vins MacBook <vinavarghese@gmail.com>
2020-11-13 09:13:33 -0800

    tree notes added

---
+++ trees.js
@@ -0,0 +1,20 @@
+// TREE NOTES:
+// - Data structure that consists of nodes in a parent / child relationships
+// - Lists are linear but trees are nonlinear (they can have more than one branch from a node)
+// - Children can't point to their siblings in a tree (i.e. only pointing from parents to children)
+
+// Definitions:
+// - ROOT - top node in a tree
+// - CHILD - a node directly connected to another node moving away from the root
+// - PARENT - converse notion of a child
+// - SIBILINGS - a group of nodes with the same parent
+// - LEAF - a node with no children (a child can be a lead but a lead is never a parent)
+// - EDGE - the actual connect between one node and another (like a branch)
+
+// Use cases:
+// - HTML DOM
+// - Network routing
+// - Abstract syntax trees
+// - AI
+// - Computer file systems
+
