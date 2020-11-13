3b98ccd9090025f71cb073f081a3488f447b2a83
Vins MacBook <vinavarghese@gmail.com>
2020-11-13 10:58:14 -0800

    tree traversal notes added

--- trees.js
+++ trees.js
@@ -7,14 +7,20 @@
 // - ROOT - top node in a tree
 // - CHILD - a node directly connected to another node moving away from the root
 // - PARENT - converse notion of a child
-// - SIBILINGS - a group of nodes with the same parent
+// - SIBLINGS - a group of nodes with the same parent
 // - LEAF - a node with no children (a child can be a lead but a lead is never a parent)
 // - EDGE - the actual connect between one node and another (like a branch)
 
-// Use cases:
+// Use Cases:
 // - HTML DOM
 // - Network routing
 // - Abstract syntax trees
 // - AI
 // - Computer file systems
 
+// Tree Traversal:
+// - Breadth-first Search
+    // - Working across the witdh of the tree
+// - Depth-first Search
+    // - Going down to the end of the branch and then back up and next
+    // - Three versions (InOrder / PreOrder / PostOrder)
