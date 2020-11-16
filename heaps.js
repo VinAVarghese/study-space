34134e9e65e0fdd4aefe823b6c90961e24ae860c
Vinny V <VinAVarghese@gmail.com>
2020-11-16 11:57:39 -0800

    adding heaps notes

---
+++ heaps.js
@@ -0,0 +1,17 @@
+// HEAPS NOTES:
+// - Heaps are a catagory of trees
+    // - All rules that apply to trees apply to heaps
+// - Binary Heaps
+    // - Similar to a binary search tree
+    // - Max two child nodes (binary)
+    // - Common Use Cases
+        // - Binary Heaps are used to implement Priority Queues
+        // - Binary Heaps are used with graph traversal algos
+// - Max Binary Heap: All children are smaller than a parent
+    // - Parent nodes are always great than it's children
+    // - No gaurentees between children (no implied ordering between siblings)
+    // - As compact as possible (talks up the least amount of space)
+        // - Both possible nodes are filled before going to the next child
+// - Min Binary Heap: All children are larger than a parent
+    // - Parent nodes are always smaller than it's children
+    // - No gaurentees between children (no implied ordering between siblings)
