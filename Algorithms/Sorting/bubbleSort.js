1b0218bfc0888ccd119b744e242857b2136ce774
Vins MacBook <vinavarghese@gmail.com>
2020-11-17 13:21:12 -0800

    adding bubble sort

---
+++ Algorithms/Sorting/bubbleSort.js
@@ -0,0 +1,22 @@
+// Bubble Sort:
+    // - The largest values "bubble" up to the top
+    // Big O:
+        // If nearly sorted array O(n)
+        // If completely random array O(n^2)
+
+const bubbleSort = (arr) => {
+    let noSwaps;
+    for (let i = arr.length; i > 0; i--) {
+        noSwaps = true;
+        for (let j = 0; j < i - 1; j++) {
+            if (arr[j] > arr[j+1]){
+                [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+                noSwaps = false;
+            }
+        }
+        if (noSwaps) break;
+    }
+    return console.log("bubbleSortedArr", arr);
+}
+
+bubbleSort([37,45,29,-3,88,8]) // [ -3, 8, 29, 37, 45, 88 ]
