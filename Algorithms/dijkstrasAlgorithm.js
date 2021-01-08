f3bff6e8f7d0c91afdf79b04bedaf39cbddba1e6
Vins MacBook <vinavarghese@gmail.com>
2021-01-07 17:13:26 -0800

    adding more of dijkstras algo

--- Algorithms/dijkstrasAlgorithm.js
+++ Algorithms/dijkstrasAlgorithm.js
@@ -17,6 +17,39 @@ class WeightedGraph {
         this.adjacencyList[v1].push({vertex:v2, weight});
         this.adjacencyList[v2].push({vertex:v1, weight});
     }
+
+    dijkstras(start, end){
+        const nodes = new PriorityQueue();
+        let distances = {};
+        let previous = {};
+
+        for(let vertex in this.adjacencyList){
+            if (vertex === start){
+                distances[vertex] = 0;
+            }
+        }
+
+        while(nodes !== null){
+
+        }
+    } 
+}
+
+// Priority Queue Implementation (needed for Dijkstras algorithim to work)
+class PriorityQueue {
+    constructor(){
+        this.values = [];
+    }
+    enqueue(val, priority) { 
+        this.values.push({val, priority});
+        this.sort();
+    };
+    dequeue() { 
+        return this.values.shift();
+    };
+    sort() {
+        this.values.sort((a,b) => a.priority - b.priority);
+    }
 }
 
 let wG = new WeightedGraph;
@@ -27,8 +60,10 @@ wG.addVertex("Boise")
 wG.addVertex("Columbus")
 wG.addVertex("Charlotte")
 wG.addEdge("Seattle", "NewYork", 8)
-wG.addEdge("Seattle", "Boise", 6)
-wG.addEdge("Seattle", "Columbus", 2)
-wG.addEdge("Seattle", "Austin", 4)
-wG.addEdge("Seattle", "Charlotte", 2)
+wG.addEdge("Columbus", "Boise", 6)
+wG.addEdge("NewYork", "Columbus", 2)
+wG.addEdge("Charlotte", "Austin", 4)
+wG.addEdge("Boise", "Charlotte", 2)
+wG.addEdge("Austin", "NewYork", 3)
 console.log(wG);
+wG.dijkstras("Seattle", "Charlotte")
