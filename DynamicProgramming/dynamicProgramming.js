241fe42058705930ba1449db69d04f55837f230b
Vinny V <VinAVarghese@gmail.com>
2021-01-13 11:56:06 -0800

    adding dynamic programming section and inital notes

---
+++ DynamicProgramming/dynamicProgramming.js
@@ -0,0 +1,9 @@
+// Dynamic Programming - A method for solving a complex problem by breaking it down into a collection of simpler subproblems, solving each of those subproblems just once, and storing their solutions. 
+
+    // Indicator that Dynamic Programming can be used? 1/2: Overlapping Subproblems
+
+// Overlapping Subproblems - A problem is said to have overlapping subproblems if it can be broken down into subproblems which are reused several times
+
+    // Indicator that Dynamic Programming can be used? 2/2: Optimal Substructure
+
+// Optimal Substructure - A problem where an optimal solution can be constructed from optimal solutions of its subproblems
